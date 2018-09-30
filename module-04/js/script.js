0f924964ed9a2d3ebd10a219eaadb061d22b9631
maksymsobipan <sobimaks@gmail.com>
2018-09-30 22:12:52 +0300

    add sm

---
+++ module-04/js/script.js
@@ -0,0 +1,130 @@
+'use strict'
+
+
+const products = {
+  bread: 10,
+  milk: 15,
+  apples: 20,
+  chicken: 50,
+  cheese: 40,
+};
+
+function Cashier(name, productDatabase, customerMoney = 0) {
+  this.customerMoney = customerMoney;
+  this.name = name;
+  this.productDatabase = productDatabase;
+  // this.TotalPrice = 0;
+  this.getCustomerMoney = function (value) {
+    return this.customerMoney = value;
+  }
+  this.countTotalPrice = function (order) {
+    let gotProductsPrice = [];
+    for (let ord in order) {
+        for (let prod in this.productDatabase) {
+          if (prod === ord) {
+          gotProductsPrice.push((order[ord]*this.productDatabase[prod]));
+        }
+      }
+    }
+    return gotProductsPrice.reduce((acc,el) => (acc += el),0);
+};
+
+const totalPrice = this.countTotalPrice(order);
+
+this.countChange = function () {
+  if (this.customerMoney >= totalPrice) {
+
+    return this.customerMoney - totalPrice;
+    // this.onSuccess
+  } else {
+    return null;
+  }
+};
+
+const change = this.countChange();
+
+
+  this.onSuccess = function (change) {
+    return console.log(`Спасибо за покупку, ваша сдача ${change}`);
+  };
+
+
+
+
+  if(change !== null) {
+     // При успешном обслуживании вызываем метод onSuccess
+    this.onSuccess(change); // Спасибо за покупку, ваша сдача 190
+  } else {
+    // При неудачном обслуживании вызываем метод onError
+    this.onError = function () {
+      return console.log(`Очень жаль, вам не хватает денег на покупки`);
+    }
+  };
+
+   // При успешном обслуживании вызываем метод onSuccess
+};
+// else {
+//   // При неудачном обслуживании вызываем метод onError
+//   mango.onError(); // Очень жаль, вам не хватает денег на покупки
+// }
+// console.log(change);
+
+
+
+
+
+  // ===========================================================
+
+/* Заказ пользователя хранится в виде объекта следующего формата. "имя-продукта":"количество-единиц" */
+const order = {
+  bread: 2,
+  milk: 2,
+  apples: 1,
+  cheese: 1
+};
+
+/* Пример использования */
+
+
+const mango = new Cashier('Mango', products);
+
+// Проверяем исходные значения полей
+console.log(mango.name); // Mango
+console.log(mango.productDatabase); // ссылка на базу данных продуктов (объект products)
+console.log(mango.customerMoney); // 0
+
+
+// Вызываем метод countTotalPrice для подсчета общей суммы
+// передавая order - список покупок пользователя
+const totalPrice = mango.countTotalPrice(order);
+//
+// // Проверям что посчитали
+console.log(totalPrice); // 110
+//
+// // Вызываем getCustomerMoney для запроса денег покупателя
+mango.getCustomerMoney(100);
+//
+// // Проверяем что в поле с деньгами пользователя
+console.log(mango.customerMoney); // 300
+//
+// // Вызываем countChange для подсчета сдачи
+const change = mango.countChange();
+//
+// // Проверяем что нам вернул countChange
+console.log(change !== null);
+console.log(`change ${change}`); // 190
+// mango.onSuccess(change);
+// // Проверяем результат подсчета денег
+// if(change !== null) {
+//    // При успешном обслуживании вызываем метод onSuccess
+//   mango.onSuccess(change); // Спасибо за покупку, ваша сдача 190
+// } else {
+//   // При неудачном обслуживании вызываем метод onError
+//   mango.onError(); // Очень жаль, вам не хватает денег на покупки
+// }
+//
+// // Вызываем reset при любом исходе обслуживания
+// mango.reset();
+//
+// // Проверяем значения после reset
+// console.log(mango.customerMoney); // 0
---
+++ module-06/js/script.js
@@ -0,0 +1,26 @@
+'use strict'
+
+// class Hamburger {
+//   // /**
+//   //  * @constructor
+//   //  * @param {String} size - Размер
+//   //  * @param {String} stuffing - Начинка
+//   //  */
+//   constructor(size, stuffing) {
+//     this._size = size;
+//     this._stuffing = stuffing;
+//     this._toppings = [];
+//   }
+// }
+//
+// Hamburger.SIZE_SMALL = 'SIZE_SMALL';
+// // Hamburger.SIZE_LARGE = ...
+//
+// Hamburger.SIZES = {
+//   SIZE_SMALL: {
+//     price: 30,
+//     calories: 50,
+//   }
+// };
+//
+// console.log(Hamburger.SIZES.SIZE_SMALL.price);
